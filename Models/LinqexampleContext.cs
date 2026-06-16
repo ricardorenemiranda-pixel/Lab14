9295df74c9b32cf07c1bc086f031552b46167132
ricardorenemiranda-pixel <ricardo.rene.miranda@gmail.com>
2026-06-16 15:00:54 -0500

    Fix OnConfiguring to not override injected connection

--- Models/LinqexampleContext.cs
+++ Models/LinqexampleContext.cs
@@ -24,8 +24,12 @@ public partial class LinqexampleContext : DbContext
     public virtual DbSet<Product> Products { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=LINQExample;Username=postgres;Password=arapa339");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=LINQExample;Username=postgres;Password=arapa339");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
